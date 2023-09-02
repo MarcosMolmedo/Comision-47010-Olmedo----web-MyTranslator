017df2279ff0773a87ed31f3bdc5879390515bf5
¨Marcos <marcosmolmedo@gmail.com>
2023-09-02 23:52:11 +0200

    se agregan funciones al html cotizaciones, se agrega interaciones con javaScript para mejor experiencia del usuario

--- main.js
+++ main.js
@@ -1,92 +1,182 @@
-/*estilo video inicial del index*/
-/* video-control.js*/
-function controlarVideo() {
-  var video = document.querySelector(".video");
 
-  video.addEventListener("loadedmetadata", function() {
-      video.currentTime = 3; 
-      video.play(); 
-  });
+        document.addEventListener("DOMContentLoaded", function() {
+            const clasesBtn = document.getElementById("clasesBtn");
+            const traduccionesBtn = document.getElementById("traduccionesBtn");
 
-  video.addEventListener("ended", function() {
-      video.currentTime = 3;
-      video.play(); 
-  });
-}
+            clasesBtn.addEventListener("click", function() {
+                realizarCotizacionClases();
+            });
 
-/* interacion para html cotizaciones*/
-function realizarInteraccion() {
-  let nombre = prompt("Ingrese su nombre");
-  alert("Bienvenido/a a My Translator, " + nombre);
+            traduccionesBtn.addEventListener("click", function() {
+                realizarCotizacionTraducciones();
+            });
 
-  alert("¡Soy una lingüista y traductora. Responde algunas preguntas para conocer qué servicios te interesan.");
+            function realizarCotizacionClases() {
+                Swal.fire({
+                    title: "¿Qué tipo de clase desea?",
+                    input: "select",
+                    inputOptions: {
+                        '1': 'Clases Individuales',
+                        '2': 'Clases Grupales'
+                    },
+                    showCancelButton: true,
+                    confirmButtonText: "Aceptar",
+                    cancelButtonText: "Cancelar",
+                }).then((tipoClaseResult) => {
+                    if (tipoClaseResult.isConfirmed) {
+                        const tipoClase = tipoClaseResult.value;
 
-  function preguntarServicio(servicio) {
-      return prompt(`¿Necesitas servicios de ${servicio}? (Si/No)`).toLowerCase() === 'si';
-  }
+                        Swal.fire({
+                            title: "Seleccione el paquete deseado:",
+                            input: "select",
+                            inputOptions: {
+                                '1': 'Paquete de 5 clases',
+                                '2': 'Paquete de 10 clases',
+                                '3': 'Paquete de 15 clases'
+                            },
+                            showCancelButton: true,
+                            confirmButtonText: "Aceptar",
+                            cancelButtonText: "Cancelar",
+                        }).then((paqueteResult) => {
+                            if (paqueteResult.isConfirmed) {
+                                const paquete = paqueteResult.value;
+                                const { costo, descripcion, cantidadClases } = calcularCostoClase(tipoClase, paquete);
 
-  let contador = 0;
-  const servicios = ["traducción", "interpretación", "clases de inglés"];
-  const intereses = [];
+                                Swal.fire({
+                                    title: "Resumen Final",
+                                    html: `Tipo de Clase: ${descripcion}<br>Cantidad de Clases: ${cantidadClases}<br>Costo Total: ${costo} euros`,
+                                    confirmButtonText: "Aceptar",
+                                });
+                            } else if (paqueteResult.dismiss === Swal.DismissReason.cancel) {
+                                Swal.fire("Acción cancelada.", "", "info");
+                            }
+                        });
+                    } else if (tipoClaseResult.dismiss === Swal.DismissReason.cancel) {
+                        Swal.fire("Acción cancelada.", "", "info");
+                    }
+                });
+            }
 
-  while (contador < servicios.length) {
-      if (preguntarServicio(servicios[contador])) {
-          intereses.push(servicios[contador]);
-          alert(`Ofrezco servicios de ${servicios[contador]} profesional en inglés y español`);
-      }
-      contador++;
-  }
+            function realizarCotizacionTraducciones() {
+                Swal.mixin({
+                    progressSteps: ['1', '2', '3', '4', '5', '6'],
+                }).queue([
+                    {
+                        title: '¿De qué idioma a qué idioma desea traducir?',
+                        input: 'select',
+                        inputOptions: {
+                            '1': 'Inglés a Español',
+                            '2': 'Español a Inglés'
+                        },
+                    },
+                    {
+                        title: '¿Qué tipo de documento desea traducir?',
+                        input: 'select',
+                        inputOptions: {
+                            '1': 'Jurídico',
+                            '2': 'Otros'
+                        },
+                    },
+                    {
+                        title: '¿A dónde va a presentar la traducción?',
+                        input: 'select',
+                        inputOptions: {
+                            '1': 'Municipalidad',
+                            '2': 'Centro Educativo',
+                            '3': 'Otros'
+                        },
+                    },
+                    {
+                        title: '¿Para cuándo necesita la traducción?',
+                        input: 'select',
+                        inputOptions: {
+                            '1': '7 días',
+                            '2': '14 días',
+                            '3': '+21 días'
+                        },
+                    },
+                    {
+                        title: '¿Cuántas fojas tiene que traducir?',
+                        input: 'select',
+                        inputOptions: {
+                            '1': '1',
+                            '2': '2',
+                            '3': '3',
+                            '4': '4',
+                            '5': '5',
+                            '6': '6',
+                            '7': '7',
+                            '8': '8',
+                            '9': '9',
+                            '10': '10'
+                        },
+                    },
+                    {
+                        title: '¿Necesita apostillar su documento?',
+                        input: 'select',
+                        inputOptions: {
+                            '1': 'Sí',
+                            '2': 'No'
+                        },
+                    }
+                ]).then((result) => {
+                    if (result.value) {
+                        const answers = result.value;
+                        Swal.fire({
+                            title: 'Resumen de Respuestas',
+                            html: `
+                                De Idioma a Idioma: ${answers[0]}<br>
+                                Tipo de Documento: ${answers[1]}<br>
+                                Lugar de Presentación: ${answers[2]}<br>
+                                Fecha de Necesidad: ${answers[3]}<br>
+                                Cantidad de Fojas: ${answers[4]}<br>
+                                Apostillar: ${answers[5]}
+                            `,
+                            confirmButtonText: 'Aceptar',
+                        });
+                    }
+                });
+            }
 
-  let costoClases = 0;
-  let tipoClases = "";
+            function calcularCostoClase(tipoClase, paquete) {
+                let costoPorClase = 0;
+                let cantidadClases = 0;
+                let descripcion = "";
 
-  function elegirTipoClases() {
-      tipoClases = prompt("Elige el tipo de clases: Grupales, Individuales o Personalizadas").toLowerCase();
+                if (tipoClase === '1') {
+                    costoPorClase = 25;
+                    descripcion += "Tipo de Clase: Clases Individuales";
+                } else if (tipoClase === '2') {
+                    costoPorClase = 35;
+                    descripcion += "Tipo de Clase: Clases Grupales";
+                }
 
-      switch (tipoClases) {
-          case "grupales":
-              costoClases = 15;
-              break;
-          case "individuales":
-              costoClases = 25;
-              break;
-          case "personalizadas":
-              costoClases = 30;
-              break;
-          default:
-              alert("Opción no válida, se tomará el costo de clases grupales por defecto.");
-              tipoClases = "grupales";
-              costoClases = 15;
-              break;
-      }
-  }
+                switch (paquete) {
+                    case '1':
+                        cantidadClases = 5;
+                        descripcion += " - Paquete de 5 clases.";
+                        break;
+                    case '2':
+                        cantidadClases = 10;
+                        descripcion += " - Paquete de 10 clases con 10% de descuento.";
+                        costoPorClase *= 0.9; // Aplicar descuento del 10%
+                        break;
+                    case '3':
+                        cantidadClases = 15;
+                        descripcion += " - Paquete de 15 clases con 15% de descuento.";
+                        costoPorClase *= 0.85; // Aplicar descuento del 15%
+                        break;
+                    default:
+                        descripcion += " - Opción de paquete no válida. Se calculará el precio por clase individual.";
+                        cantidadClases = 1;
+                        break;
+                }
 
-  if (intereses.includes("clases de inglés")) {
-      elegirTipoClases();
-  } else {
-      alert("No seleccionaste clases de inglés. ¡Hasta luego!");
-  }
+                const costoTotal = costoPorClase * cantidadClases;
+                descripcion += `<br>Precio por Clase: ${costoPorClase} euros`;
+                descripcion += `<br>Cantidad de Clases: ${cantidadClases}`;
+                descripcion += `<br>Costo Total: ${costoTotal} euros`;
 
-  const horariosRespuesta = {
-      lunesAViernes: "9:00 AM - 6:00 PM",
-      sabados: "10:00 AM - 2:00 PM"
-  };
-
-  let horarioSeleccionado = prompt("Selecciona un horario de respuesta: Lunes a Viernes o Sábados").toLowerCase();
-
-  if (horariosRespuesta[horarioSeleccionado]) {
-      alert(`Nuestro horario de respuesta es: ${horariosRespuesta[horarioSeleccionado]}`);
-  } else {
-      alert("Horario no válido, se tomará el horario de respuesta por defecto (Lunes a Viernes).");
-      horarioSeleccionado = "lunesAViernes";
-  }
-
-  if (intereses.length === 0) {
-      alert("No seleccionaste ningún servicio. ¡Hasta luego!");
-  } else {
-      alert(`Gracias por responder el cuestionario, ${nombre}. Has mostrado interés en los siguientes servicios: ${intereses.join(", ")}.`);
-      if (tipoClases) {
-          alert(`Resumen de tu selección:\nNombre: ${nombre}\nServicios de interés: ${intereses.join(", ")}\nTipo de clases: ${tipoClases}\nCosto de clases: ${costoClases} euros\nHorario de respuesta: ${horariosRespuesta[horarioSeleccionado]}`);
-      }
-  }
-}
+                return { costo: costoTotal, descripcion, cantidadClases };
+            }
+        });
